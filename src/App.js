e9b96d4202b6ce6469b4d7844fd42577cc3a34c4
roberjank <roberjank@gmail.com>
2022-05-04 15:48:35 +0300

    index js, app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button, Block, Hero } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Hero color="success" title="Success hero" subtitle="Success subtitle" />
+      <Hero color="danger" title="Danger hero" subtitle="Danger subtitle" />
+    </div>
+  );
 };
 
 export default App;
